d7f25884aa509c10912c5b279a654431f8173572
mlawrence <mlawrence@alsde.edu>
2019-01-30 15:40:08 -0600

    add remove members from user groups display

--- AdenDemo.Web/Controllers/api/MembershipController.cs
+++ AdenDemo.Web/Controllers/api/MembershipController.cs
@@ -23,6 +23,48 @@ namespace AdenDemo.Web.Controllers.api
             var users = _userService.GetUsers(username);
             return Ok(users.ToList());
         }
+
+        [HttpPut, Route("groupmembers")]
+        public object AddGroupUser(UpdateGroupMemberDto model)
+        {
+            //var client = new SmtpClient();
+            ////Regex.Replace(groupName.Humanize().ToTitleCase(), " App ", " ", RegexOptions.ExplicitCapture);
+            //var message = new MailMessage()
+            //{
+            //    Body = $"Please ADD user {model.Email} to group <br />{ Regex.Replace(model.GroupName.Humanize().ToTitleCase().TrimEnd('s'), " App ", " ", RegexOptions.ExplicitCapture) }",
+            //    To = { "helpdesk@alsde.edu" },
+            //    From = new MailAddress(User.Identity.Name),
+            //    IsBodyHtml = true
+            //};
+
+            //client.Send(message);
+
+            return Ok($"Added {model.Email} to {model.GroupName}");
+        }
+
+        [HttpDelete, Route("groupmembers")]
+        public object DeleteGroupMember(UpdateGroupMemberDto model)
+        {
+
+            //var client = new SmtpClient();
+            //var message = new MailMessage()
+            //{
+            //    Body = $"Please REMOVE user {model.Email} from group <br />{ Regex.Replace(model.GroupName.Humanize().ToTitleCase().TrimEnd('s'), " App ", " ", RegexOptions.ExplicitCapture) }",
+            //    To = { "helpdesk@alsde.edu" },
+            //    From = new MailAddress(User.Identity.Name),
+            //    IsBodyHtml = true
+            //};
+
+            //client.Send(message);
+
+            return Ok($"Deleted {model.Email} to {model.GroupName}");
+        }
+    }
+
+    public class UpdateGroupMemberDto
+    {
+        public string GroupName { get; set; }
+        public string Email { get; set; }
     }
 
     public class UserModel
